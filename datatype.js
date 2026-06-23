d4189e9445ff262c36f32f16f460732d948fbd0e
ghulam-zahra <ghulamzahra1504@gmail.com>
2026-06-23 06:06:17 +0000

    datatyes

--- datatype.js
+++ datatype.js
@@ -22,6 +22,34 @@ console.log(valueInNumber); // 1 if false then 0
 let boolean = Boolean(isLoogedIn)
 console.log(boolean); // true
 
+// Primitive Data types
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('345');
+const anotherId = Symbol('345')
+
+console.log(id === anotherId) // false bcz if symbol has same value but it shows false
+
+const bigNumber = 3333344444555555666667777777777999999000n
+
+// Reference (Non primitive)
+// Array , Objects, Functions
+const heros = ["Iqbal", "Liaqat Ali Khan"] // arr
+console.log(typeof heros);
+
+let obj = {
+    name: "zahra",
+    age: 21,
+} // objects
+console.log(typeof obj);
+
+
+const myFunc = function(){
+    console.log("hi");
+    
+}// function
+console.log(typeof myFunc);
+
 
 
 
