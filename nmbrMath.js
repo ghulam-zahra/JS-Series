70b4f3c159fce6711ab65d7c67065c987aa3a050
ghulam-zahra <ghulamzahra1504@gmail.com>
2026-06-23 06:05:24 +0000

    number and math

---
+++ nmbrMath.js
@@ -0,0 +1,24 @@
+const  price = new Number(100)
+console.log(price);
+console.log(price.toString().length)
+console.log(price.toFixed(3));
+
+const otherNumber = 13.89076
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString())
+
+// **************** Maths ****************
+console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(10.3));
+console.log(Math.ceil(4.2));// always choose next value
+console.log(Math.floor(4.9)); // ignore point value
+console.log(Math.min(3,5,7))
+console.log(Math.max(78,65,89));
+console.log(Math.random()*10);
+
+
+
+
